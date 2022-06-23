6c2d8c2947f8276bb816538d4039639e7fb8b2c6
Sean-Tisdale <stisdale724@gmail.com>
2022-06-23 13:58:10 -0700

    updated logic

--- src/components/OnSalePage/forSalePage.tsx
+++ src/components/OnSalePage/forSalePage.tsx
@@ -47,124 +47,132 @@ function ForSalePage(props: any) {
     <>
       <NavBar />
       <div className={styles.nftPageWrapper}>
-        {nftSellOrders?.map(
-          (data: any) =>
-            props?.props?.token ===
-              data?.asset?.asset_contract?.address +
-                data?.asset?.token_id +
-                data?.id && (
-              <>
-                <div key={props?.props?.token} className={styles.cardWrapper}>
-                  <img className={styles.image} src={data?.asset?.image_url} />
-
-                  <div>
-                    <div className={styles.descriptionHeader}>Description</div>
-                    <div className={styles.description}>
-                      {data?.asset?.description}
-                    </div>
-                  </div>
-                </div>
-                <div className={styles.infoWrapper}>
-                  <div className={styles.infoTopWrapper}>
-                    <Link
-                      href={`/CollectionPage/${data?.asset?.collection?.slug}`}
-                    >
-                      <div className={styles.collection}>
-                        {data?.asset?.collection?.name}
-                        <span className={styles.checkmark}></span>
-                      </div>
-                    </Link>
-                    <div className={styles.name}>{data?.asset?.name}</div>
-                    <div className={styles.ownerWrapper}>
-                      <div>Owned by &nbsp;</div>
-                      <a
-                        href={`https://etherscan.io/address/${data?.asset?.owner?.address}`}
-                        target='_blank'
-                        rel='noreferrer'
-                        className={styles.ownerAddress}
-                      >
-                        {data?.asset?.owner?.address?.substring(0, 4)}...
-                        {data?.asset?.owner?.address?.substring(
-                          data?.asset?.owner?.address?.length - 4
-                        )}
-                        &nbsp;
-                        <span className={styles.checkmark}></span>
-                      </a>
-                    </div>
-                  </div>
-                  <div className={styles.buttonWrapper}>
-                    <div>
-                      Price: {ethers.utils.formatEther(data?.base_price)} ETH
-                    </div>
-                    <button className={styles.button} onClick={handleClick}>
-                      Buy Now
-                    </button>
-                    <button className={styles.button} onClick={handleClick}>
-                      Make Offer
-                    </button>
-                    <input
-                      className={styles.priceInput}
-                      onChange={handleChange}
-                      placeholder='Amount'
+        {nftSellOrders &&
+          nftSellOrders?.map(
+            (data: any) =>
+              props?.props?.token ===
+                data?.asset?.asset_contract?.address +
+                  data?.asset?.token_id +
+                  data?.id && (
+                <>
+                  <div key={props?.props?.token} className={styles.cardWrapper}>
+                    <img
+                      className={styles.image}
+                      src={data?.asset?.image_url}
                     />
-                  </div>
-                  <div>
-                    <div
-                      className={
-                        display ? styles.detailsHeader : styles.detailsHeader1
-                      }
-                      onClick={() => setDisplay(!display)}
-                    >
-                      Details
-                      <div className={display ? 'none' : styles.arrow}>^</div>
-                    </div>
-                    <div
-                      className={
-                        display ? styles.detailsInfo : styles.detailsInfoHidden
-                      }
-                    >
-                      <div>
-                        Token ID:&nbsp;
-                        {(tokenID = data?.asset?.token_id)}
-                      </div>
-                      <div style={{ display: 'none' }}>
-                        {
-                          (tokenToBuyAddress =
-                            data?.asset?.asset_contract?.address)
-                        }
-                      </div>
-                      <div style={{ display: 'none' }}>
-                        {
-                          (tokenToBuyAddress =
-                            data?.asset?.asset_contract?.address)
-                        }
-                      </div>
-                      <div>
-                        Contract Address:&nbsp;
-                        {data?.asset?.asset_contract?.address.substring(0, 4)}
-                        ...
-                        {data?.asset?.asset_contract?.address?.substring(
-                          data?.asset?.asset_contract?.address?.length - 4
-                        )}
-                      </div>
-                      <div>
-                        Token Type: {data?.asset?.asset_contract?.schema_name}
-                      </div>
 
-                      <a
-                        href={data?.asset?.permalink}
-                        target='_blank'
-                        rel='noreferrer'
-                        className={styles.linkTag}
-                      >
-                        OpenSea Link
-                      </a>
+                    <div>
+                      <div className={styles.descriptionHeader}>
+                        Description
+                      </div>
+                      <div className={styles.description}>
+                        {data?.asset?.description}
+                      </div>
                     </div>
                   </div>
-                </div>
-              </>
-            )
-        )}
+                  <div className={styles.infoWrapper}>
+                    <div className={styles.infoTopWrapper}>
+                      <Link
+                        href={`/CollectionPage/${data?.asset?.collection?.slug}`}
+                      >
+                        <div className={styles.collection}>
+                          {data?.asset?.collection?.name}
+                          <span className={styles.checkmark}></span>
+                        </div>
+                      </Link>
+                      <div className={styles.name}>{data?.asset?.name}</div>
+                      <div className={styles.ownerWrapper}>
+                        <div>Owned by &nbsp;</div>
+                        <a
+                          href={`https://etherscan.io/address/${data?.asset?.owner?.address}`}
+                          target='_blank'
+                          rel='noreferrer'
+                          className={styles.ownerAddress}
+                        >
+                          {data?.asset?.owner?.address?.substring(0, 4)}...
+                          {data?.asset?.owner?.address?.substring(
+                            data?.asset?.owner?.address?.length - 4
+                          )}
+                          &nbsp;
+                          <span className={styles.checkmark}></span>
+                        </a>
+                      </div>
+                    </div>
+                    <div className={styles.buttonWrapper}>
+                      <div>
+                        Price: {ethers.utils.formatEther(data?.base_price)} ETH
+                      </div>
+                      <button className={styles.button} onClick={handleClick}>
+                        Buy Now
+                      </button>
+                      <button className={styles.button} onClick={handleClick}>
+                        Make Offer
+                      </button>
+                      <input
+                        className={styles.priceInput}
+                        onChange={handleChange}
+                        placeholder='Amount'
+                      />
+                    </div>
+                    <div>
+                      <div
+                        className={
+                          display ? styles.detailsHeader : styles.detailsHeader1
+                        }
+                        onClick={() => setDisplay(!display)}
+                      >
+                        Details
+                        <div className={display ? 'none' : styles.arrow}>^</div>
+                      </div>
+                      <div
+                        className={
+                          display
+                            ? styles.detailsInfo
+                            : styles.detailsInfoHidden
+                        }
+                      >
+                        <div>
+                          Token ID:&nbsp;
+                          {(tokenID = data?.asset?.token_id)}
+                        </div>
+                        <div style={{ display: 'none' }}>
+                          {
+                            (tokenToBuyAddress =
+                              data?.asset?.asset_contract?.address)
+                          }
+                        </div>
+                        <div style={{ display: 'none' }}>
+                          {
+                            (tokenToBuyAddress =
+                              data?.asset?.asset_contract?.address)
+                          }
+                        </div>
+                        <div>
+                          Contract Address:&nbsp;
+                          {data?.asset?.asset_contract?.address.substring(0, 4)}
+                          ...
+                          {data?.asset?.asset_contract?.address?.substring(
+                            data?.asset?.asset_contract?.address?.length - 4
+                          )}
+                        </div>
+                        <div>
+                          Token Type: {data?.asset?.asset_contract?.schema_name}
+                        </div>
+
+                        <a
+                          href={data?.asset?.permalink}
+                          target='_blank'
+                          rel='noreferrer'
+                          className={styles.linkTag}
+                        >
+                          OpenSea Link
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )
+          )}
       </div>
     </>
   )
