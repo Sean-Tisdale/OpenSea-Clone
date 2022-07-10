078aa86adff2f4d6f9f7b4de3866e67465d33cbe
Sean-Tisdale <stisdale724@gmail.com>
2022-07-10 18:26:09 -0400

    updated logic

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,11 +1,13 @@
-import React from "react"
+import React from 'react'
 import type { NextPage } from 'next'
-import UserProfile from "../../components/UserProfile/userProfile"
-
+import UserProfile from '../../components/UserProfile/userProfile'
 
 const ProfilePage: NextPage = () => {
-    return <UserProfile/>
-    
+  return (
+    <>
+      <UserProfile />
+    </>
+  )
 }
 
 export default ProfilePage
